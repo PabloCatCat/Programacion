73d8055dd0a1376ac5dd3252a2fbb66b7fe8f833
Pablo <pabherala2@alu.edu.gva.es>
2024-11-22 03:04:04 +0100

    cambio tragaperras

--- Unidad 5/Ejercicios/Actividades/Aleatorio16.java
+++ Unidad 5/Ejercicios/Actividades/Aleatorio16.java
@@ -2,47 +2,31 @@ import java.util.Scanner;
 
 public class Aleatorio16 {
     public static void main(String[] args) {
+        Aleatorio16 programa=new Aleatorio16();
+        programa.inicio();
+    }
 
-        Scanner sc = new Scanner(System.in);
-        int figuraNum, euros;
-        String figura = "", figura1 = "", figura2 = "", figura3 = "", respuesta = "";
+    private void inicio() {
+        int euros;
+        String figura1 = "", figura2 = "", figura3 = "", respuesta = "";
 
-        do {
-            System.out.print("Introduce euros: ");
-            euros = sc.nextInt();
-        }while(euros<=0);
+        euros = getEuros();
 
         System.out.println();
 
         do {
             euros--;
             for (int tir = 1; tir <= 3; tir++) {
-                figuraNum = (int) (Math.random() * 5 + 1);
-                switch (figuraNum) {
-                    case 1:
-                        figura = "corazón";
-                        break;
-                    case 2:
-                        figura = "diamante";
-                        break;
-                    case 3:
-                        figura = "herradura";
-                        break;
-                    case 4:
-                        figura = "campana";
-                        break;
-                    case 5:
-                        figura = "limón";
-                        break;
-                }
-                if (tir == 1)
-                    figura1 = figura;
-                else if (tir == 2)
-                    figura2 = figura;
+                if (tir==1)
+                    figura1=getFigura();
+                else if (tir==2)
+                    figura2=getFigura();
                 else
-                    figura3 = figura;
+                    figura3=getFigura();
             }
+
             System.out.println(figura1 + " " + figura2 + " " + figura3);
+
             if (figura1.equals(figura2) && figura1.equals(figura3)){
                 System.out.println("Enhorabuena, has ganado 10 euros");
                 euros+=10;
@@ -57,8 +41,7 @@ public class Aleatorio16 {
             System.out.println("Tienes " + euros + "€");
             if(euros>0){
                 do {
-                    System.out.println("¿Quieres continuar?");
-                    respuesta=sc.next();
+                    respuesta=getRespuesta();
                     if (!respuesta.equalsIgnoreCase("si")&&!respuesta.equalsIgnoreCase("no"))
                         System.out.println("Error, escribe si o no");
                 }while(!respuesta.equalsIgnoreCase("si")&&!respuesta.equalsIgnoreCase("no"));
@@ -66,5 +49,42 @@ public class Aleatorio16 {
             }
         }while (euros > 0&&respuesta.equalsIgnoreCase("si"));
         System.out.println("Has acabado con " + euros + "€");
+    }
+
+    private static String getRespuesta() {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("¿Quieres continuar?");
+        return sc.next();
+    }
+
+    private static String getFigura() {
+        int figuraNum;
+
+        figuraNum = (int)(Math.random() * 6);
+
+        switch (figuraNum) {
+            case 1:
+                return "corazón";
+            case 2:
+                return "diamante";
+            case 3:
+                return "herradura";
+            case 4:
+                return "campana";
+            default:
+                return "limón";
+        }
+    }
+
+    private static int getEuros() {
+        Scanner sc = new Scanner(System.in);
+        int euros;
+
+        do {
+            System.out.print("Introduce euros: ");
+            euros = sc.nextInt();
+        }while(euros<=0);
+        return euros;
     }
 }
