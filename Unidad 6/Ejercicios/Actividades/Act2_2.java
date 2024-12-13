9c0b60dd543cd7c44f8e56e522eb9c8e8122aa3b
Pablo <pabherala2@alu.edu.gva.es>
2024-12-13 17:29:59 +0100

    rebuild proyecto y ej 2_2

--- Unidad 6/Ejercicios/Actividades/Act2_2.java
+++ Unidad 6/Ejercicios/Actividades/Act2_2.java
@@ -6,24 +6,61 @@ public class Act2_2 {
 
     private void inicio() {
         int[][] num = getArrayBi(4,5);
-        int[] suma = getArray(num[0].length);
+        int[] sumaFilas = getArray(4), sumaColumnas=getArray(5);
+        int total=0;
 
         rellenarArray(num);
-        printArray(num);
+        total=getSumas(num,sumaColumnas,sumaFilas,total);
+        printArray(num,sumaFilas,sumaColumnas,total);
     }
+
+    private int getSumas(int[][] num, int[] sumaColumnas, int[] sumaFilas, int total) {
+        getSumaFilasColumnas(num,sumaFilas,sumaColumnas);
+        total=getTotal(total,sumaFilas,sumaColumnas);
+        return total;
+    }
+
+    private int getTotal(int total, int[] sumaFilas, int[] sumaColumnas) {
+        for (int i = 0; i < sumaFilas.length; i++) {
+            total+=sumaFilas[i];
+        }
+        for (int i = 0; i < sumaColumnas.length; i++) {
+            total+=sumaColumnas[i];
+        }
+        return total;
+    }
+
+    private void getSumaFilasColumnas(int[][] num, int[] sumaFilas, int[] sumaColumnas) {
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num[i].length; j++) {
+                sumaFilas[i] += num[i][j];
+                sumaColumnas[j] += num[i][j];
+            }
+
+        }
+    }
+
 
     private int[] getArray(int longitud) {
         return new int[longitud];
     }
 
-    private void printArray(int[][] num) {
-        int suma;
+    private void printArray(int[][] num,int[] sumaFilas, int[] sumaColumnas, int total) {
 
         for (int i=0;i< num.length;i++){
             for (int j=0;j<num[i].length;j++){
-                printPantalla
+            printPantalla(num[i][j] + " | ");
             }
+            printPantalla(sumaFilas[i] + " |\n");
         }
+        for (int i = 0; i < sumaColumnas.length; i++) {
+            printPantalla(sumaColumnas[i] + " | ");
+        }
+        printPantalla(total +" |\n");
+    }
+
+    private void printPantalla(String s) {
+        System.out.print(s);
     }
 
     private void rellenarArray(int[][] num) {
